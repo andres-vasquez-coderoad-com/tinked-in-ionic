3dcbb4d63f799b82dd627b32a5ce6de15b88f2c2
Andres Vasquez <andres.vasquez.agramont@gmail.com>
2021-04-26 00:01:28 -0400

    Prepare project for TDD

--- src/services/recommendation/recommendation.service.spec.ts
+++ src/services/recommendation/recommendation.service.spec.ts
@@ -1,16 +1,44 @@
 import {TestBed} from '@angular/core/testing';
 
 import {RecommendationService} from './recommendation.service';
+import {RecommendationModel} from '../../model/recommendation.model';
+import {CandidateUserModel, UserModel} from '../../model/user.model';
 
 describe('RecommendationService', () => {
-  let service: RecommendationService;
+    let service: RecommendationService;
+    let recommentation: RecommendationModel;
+    const defaultUser: UserModel = new CandidateUserModel(
+        '123',
+        'Juan Perez',
+        'test@upb.edu',
+        'Bolivia',
+        'La Paz',
+        [],
+        true
+    );
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(RecommendationService);
-  });
+    beforeEach(done => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(RecommendationService);
+        service.getRecommendations(defaultUser).subscribe(
+            data => {
+                recommentation = data;
+                done();
+            },
+            error => {
+                console.log('Error loading the Data');
+                done();
+            });
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('1. First step', () => {
+        expect(recommentation).not.toBeNull();
+        expect(2 + 2).toBe(4);
+        expect(service).toBeTruthy();
+    });
+
+    it('2. Second step', () => {
+        expect(2 + 3).toBe(5);
+        expect(service).toBeTruthy();
+    });
 });
